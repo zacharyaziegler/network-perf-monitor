adc586e5dafb6fbbfc1bff5e3d001f4ec7c2528b
zacharyaziegler <zacharyaziegler@gmail.com>
2025-06-26 15:03:41 -0400

    chore(backend): add Docker Compose servics for TimescaleDB and containerized backend

--- backend/src/main/java/com/example/networkperf/backend/BackendApplication.java
+++ backend/src/main/java/com/example/networkperf/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.example.networkperf.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EntityScan("com.example.networkperf.backend.model")
 public class BackendApplication {
 
 	public static void main(String[] args) {
